a09bc153d167e12634ab29bc12806038e8108d5c
Ben Mercer <mercerbe@gmail.com>
2018-10-10 08:16:55 -0400

    Updates

---
+++ component---src-pages-page-2-js-8cc1a4b00a75562b3b53.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{139:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(144),l=t(143);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-8cc1a4b00a75562b3b53.js.map
